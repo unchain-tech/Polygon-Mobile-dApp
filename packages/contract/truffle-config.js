ffc2f4479ca9881200c649c18e70e68b6fb9e2bf
SHØ <neila@users.noreply.github.com>
2023-04-21 21:59:21 +0900

    fix truffle config

--- packages/contract/truffle-config.js
+++ packages/contract/truffle-config.js
@@ -6,7 +6,7 @@ const mnemonic = fs.readFileSync('.secret').toString().trim();
 module.exports = {
   networks: {
     development: {
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 9545,
       network_id: '*',
     },

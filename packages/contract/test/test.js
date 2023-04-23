cb2f059e2fea65422b20f08f7594e0c74cbf4684
github honganji <yujicavs@icloud.com>
2023-04-23 12:58:20 +0900

    fix creating function test

--- packages/contract/test/test.js
+++ packages/contract/test/test.js
@@ -1,28 +1,35 @@
 // eslint-disable-next-line no-undef
 const TodoContract = artifacts.require(`TodoContract`);
+const truffleAssert = require('truffle-assertions');
 
 require(`chai`).use(require('chai-as-promised')).should();
 const { expect } = require('chai');
 
-describe('Polygon-Mobile-dApp', () => {
-  it('CRUD is working on chain', async () => {
-    const contract = await TodoContract.new();
+// eslint-disable-next-line no-undef
+contract('Polygon-Mobile-dApp', () => {
+  // declare contract variable
+  let contract;
 
-    // check if you can create a task
-    await contract.createTask('make lunch');
+  // deploy contract before all of the tests
+  before(async () => {
+    contract = await TodoContract.new();
+  });
+
+  // check create function
+  it('create function is working on chain', async () => {
+    // check if you can create multiple tasks
+    const creationTX = await contract.createTask('make lunch');
+    await contract.createTask('do the dises');
+    await contract.createTask('have luch with friends');
+
+    // check if you can read tasks
     expect((await contract.readTask(0))[1]).to.equal('make lunch');
+    expect((await contract.readTask(1))[1]).to.equal('do the dises');
+    expect((await contract.readTask(2))[1]).to.equal('have luch with friends');
 
-    // check if you can update task
-    await contract.updateTask(0, 'make dinner');
-    expect((await contract.readTask(0))[1]).to.equal('make dinner');
-
-    // check if you can make a task completed
-    const formerState = (await contract.readTask(0))[2];
-    await contract.toggleComplete(0);
-    expect((await contract.readTask(0))[2]).to.equal(!formerState);
-
-    // check if you can delete a task
-    await contract.deleteTask(0);
-    expect((await contract.readTask(0))[1]).to.equal('');
+    // check if event "TaskCreated" works
+    truffleAssert.eventEmitted(creationTX, 'TaskCreated', (ev) => {
+      return ev.taskNumber.toNumber() === 0 && ev.task === 'make lunch';
+    });
   });
 });

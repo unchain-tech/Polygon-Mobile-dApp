d2a72ff22e1a4ccbe4cacab0c9bf85548114249a
github honganji <yujicavs@icloud.com>
2023-05-27 14:38:09 +0900

    fix deploy.js and config file for hardhat

--- packages/contract/scripts/deploy.js
+++ packages/contract/scripts/deploy.js
@@ -1,31 +1,29 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require('hardhat');
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  console.log('Deploying contracts with account: ', deployer.address);
+  console.log('Account balance: ', accountBalance.toString());
 
-  const lockedAmount = hre.ethers.utils.parseEther('1');
-
-  const Lock = await hre.ethers.getContractFactory('Lock');
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`,
+  const todoContractFactory = await hre.ethers.getContractFactory(
+    'TodoContract',
   );
-}
+  /* コントラクトに資金を提供できるようにする */
+  const todoContract = await todoContractFactory.deploy();
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  await todoContract.deployed();
+
+  console.log('TodoContract address: ', todoContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+  } catch (error) {
+    console.error(error);
+    throw new Error('there is error!');
+  }
+};
+
+runMain();

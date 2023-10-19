e6960a2ba123720be2113c0af699659e45d8ea99
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-10-19 18:43:14 +0900

    feat: add flutter_dotenv

--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:client/TodoList.dart';
 import 'package:client/TodoListModel.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 

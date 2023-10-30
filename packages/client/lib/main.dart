6694f57b27788b71a204ad9ebbad10e50a6a1205
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-10-30 21:20:38 +0900

    refactor: changed filename to lowercase with underscores per style guide

--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
-import 'TodoListModel.dart';
-import 'WalletConnect.dart';
+import 'todo_list_model.dart';
+import 'wallet_connect.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
